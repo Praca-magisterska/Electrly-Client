9381190282e01b758a6a1040a69ad73ffa3d766b
jakubLabedowicz <jalabedowicz@gmail.com>
2023-12-01 16:51:19 +0100

    change

--- src/app/(module)/categories/dist/page.js
+++ src/app/(module)/categories/dist/page.js
@@ -32,7 +32,7 @@ function Signin() {
         },
         {
             imageUrl: "https://images.unsplash.com/photo-1473308822086-710304d7d30c?q=80&w=1931&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-            name: "Automatyka Przemysłowa"
+            name: "Audio i Akustyka"
         }
     ];
     return (React.createElement(Page_1["default"], null,
